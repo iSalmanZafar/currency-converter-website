4b2ce866979921456fbe1a23d4bef50c73b1b656
Salman Zafar <entityparadigm99@gmail.com>
2020-01-05 19:12:33 +0500

    changed api and implemented convert button

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
-const accessKey = "54330fe9db79ee783c6b7f6a26225a89";
-const baseUrl = "http://data.fixer.io/api/";
-let url = baseUrl + "latest?access_key=" + accessKey;
+const apiKey = "c8a660196abab7e82229";
+const baseUrl = "https://free.currconv.com/api/v7/";
 
-fetch(url)
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(err => {
-    console.log("ERROR:: " + err);
-  });
+const fromSelect = document.querySelector("#from-currency-select");
+const amount = document.querySelector("#amount");
+const toSelect = document.querySelector("#to-currency-select");
+const convertBtn = document.querySelector("#convert");
+const output = document.querySelector("#output");
+
+function convertCurrency(from, to) {
+  url = `${baseUrl}convert?q=${from}_${to}&compact=ultra&apiKey=${apiKey}`;
+  // console.log("URL:: " + url);
+  fetch(url)
+    .then(res => res.json())
+    .then(json => {
+      const conversionRate = json[`${from}_${to}`];
+      let out = conversionRate * amount.value;
+      showOutput(out);
+    })
+    .catch(err => console.log("ERROR:: " + err));
+}
+
+function showOutput(text) {
+  output.innerHTML = text;
+}
+
+convertBtn.addEventListener("click", e => {
+  e.preventDefault();
+  convertCurrency(fromSelect.value, toSelect.value);
+});
